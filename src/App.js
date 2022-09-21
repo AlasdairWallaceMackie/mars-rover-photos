ce48c11a7d64562b3eaae1c887daf36807e9d9ac
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-09-21 12:00:05 -0400

    Add RoverSelect route

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React from "react"
+import {Routes, Route} from "react-router-dom"
+import {RoverContext} from "./data/roverContext"
 
-import marsPhoto from "./img/mars.png"
+import Header from "./components/Header"
+import RoverSelect from "./components/RoverSelect"
+
 
 export default function App(){
-    return (
-        <main className="p-4 bg-space text-center text-white">
-            <header className="pt-5">
-                <div>
-                    <h1 className="header--title">Welcome to Mars</h1>
-                    <h2 className="header--subtitle">Explore photos from NASA's Mars rovers</h2>
-                    <a className="down-arrow" href="#home">
-                        <i className="ri-arrow-down-s-line ri-3x" />
-                    </a>
-                </div>
-                <img className="mt-5" src={marsPhoto}/>
-            </header>
+    const {roverData} = React.useContext(RoverContext)
+    const waitMessage = <h1 className="text-center">Fetching Data... <div className="spinner-border text-light" role="status"></div></h1>
 
-            <content id="home">
-                <h1>Home</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at interdum quam. In ultricies leo a est interdum ultricies. Sed eget purus varius, porta felis sed, convallis justo. Morbi vulputate, purus non auctor scelerisque, purus tortor elementum neque, sed porttitor odio nunc quis augue. Donec augue velit, pretium at eros eu, tempus ornare enim. Integer vestibulum laoreet ex, sit amet cursus metus eleifend id. Vivamus eget pulvinar leo. Nulla nibh turpis, dignissim et eleifend nec, maximus vel arcu. Aliquam erat volutpat. Nullam laoreet metus id eleifend mattis. Sed facilisis quam quis dui varius sodales. Maecenas tellus elit, sodales eu vulputate sit amet, pulvinar vitae dolor. Nam condimentum sodales ultricies. Nunc congue ullamcorper orci sed venenatis. Nulla hendrerit ac eros vel porttitor.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at interdum quam. In ultricies leo a est interdum ultricies. Sed eget purus varius, porta felis sed, convallis justo. Morbi vulputate, purus non auctor scelerisque, purus tortor elementum neque, sed porttitor odio nunc quis augue. Donec augue velit, pretium at eros eu, tempus ornare enim. Integer vestibulum laoreet ex, sit amet cursus metus eleifend id. Vivamus eget pulvinar leo. Nulla nibh turpis, dignissim et eleifend nec, maximus vel arcu. Aliquam erat volutpat. Nullam laoreet metus id eleifend mattis. Sed facilisis quam quis dui varius sodales. Maecenas tellus elit, sodales eu vulputate sit amet, pulvinar vitae dolor. Nam condimentum sodales ultricies. Nunc congue ullamcorper orci sed venenatis. Nulla hendrerit ac eros vel porttitor.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at interdum quam. In ultricies leo a est interdum ultricies. Sed eget purus varius, porta felis sed, convallis justo. Morbi vulputate, purus non auctor scelerisque, purus tortor elementum neque, sed porttitor odio nunc quis augue. Donec augue velit, pretium at eros eu, tempus ornare enim. Integer vestibulum laoreet ex, sit amet cursus metus eleifend id. Vivamus eget pulvinar leo. Nulla nibh turpis, dignissim et eleifend nec, maximus vel arcu. Aliquam erat volutpat. Nullam laoreet metus id eleifend mattis. Sed facilisis quam quis dui varius sodales. Maecenas tellus elit, sodales eu vulputate sit amet, pulvinar vitae dolor. Nam condimentum sodales ultricies. Nunc congue ullamcorper orci sed venenatis. Nulla hendrerit ac eros vel porttitor.</p>
-            </content>
+    return (
+        <main className="p-4 bg-space text-light">
+            <Header />
+
+            <Routes>
+                <Route exact path="/" element={roverData ? <RoverSelect /> : waitMessage} />
+            </Routes>
         </main>
     )
 }
---
+++ src/components/RoverSelect.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+import {RoverContext} from "../data/roverContext"
+import roverProfileImageData from "../data/roverProfileImage"
+
+import Rover from "./RoverProfile"
+
+
+export default function RoverSelect(props){
+    const {roverData} = React.useContext(RoverContext)
+    const roverElements = roverData.map(rover => (
+        <Rover 
+            key={rover.id}
+            name={rover.name}
+            img={roverProfileImageData[rover.name]}
+        />
+    ))
+
+    return (
+        <div id="rover-select" className="text-center">
+            <h1 className="rover-select--header">Select Rover:</h1>
+
+            <div className="d-flex justify-content-around flex-wrap">
+                {roverElements}
+            </div>
+        </div>
+    )
+}
