a2b15ce9839e7b58c5695beb71bd53e3c8defc02
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-09-21 11:59:31 -0400

    Data for profile images

---
+++ src/data/roverProfileImage.js
@@ -0,0 +1,15 @@
+import opportunity from "../img/rovers/opportunity.jpg"
+import spirit from "../img/rovers/spirit.jpg"
+import curiosity from "../img/rovers/curiosity.jpg"
+import perseverance from "../img/rovers/perseverance.jpg"
+import default_image from "../img/rovers/default.jpg" 
+
+const roverProfileImageData = {
+    "Opportunity": opportunity,
+    "Spirit": spirit,
+    "Curiosity": curiosity,
+    "Perseverance": perseverance,
+    default: default_image,
+}
+
+export default roverProfileImageData
