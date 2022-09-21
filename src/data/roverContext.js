16d703daf1c5267635022ec86c8a154852a6fe57
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-09-21 11:59:15 -0400

    Initial context commit

---
+++ src/data/roverContext.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+const RoverContext = React.createContext()
+
+function RoverContextProvider(props){
+    const [roverData, setRoverData] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("https://mars-photos.herokuapp.com/api/v1/rovers/")
+            .then(response => response.json())
+            .then(data => setRoverData(data))
+    }, [])
+
+    return (
+        <RoverContext.Provider value={{roverData: roverData.rovers}}>
+            {props.children}
+        </RoverContext.Provider>
+    )
+}
+
+export {RoverContextProvider, RoverContext}
