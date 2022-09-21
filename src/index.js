ca6a603a59e0f9229f0a0cfbb6edbc3c064d0c1c
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-09-21 11:58:37 -0400

    Add context and router to App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import {RoverContextProvider} from "./data/roverContext"
+import {BrowserRouter} from "react-router-dom"
 
 import App from "./App"
 import "./App.css"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(
+    <RoverContextProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </RoverContextProvider>,
+    document.getElementById("root")
+)
