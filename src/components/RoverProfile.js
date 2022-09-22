a561768ad5f61c7f5c13e35899a529769d9bf993
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-09-22 11:44:56 -0400

    Status indicator added

--- src/components/RoverProfile.js
+++ src/components/RoverProfile.js
@@ -5,15 +5,38 @@ import {RoverContext} from "../data/roverContext"
 import roverProfileImageData from "../data/roverProfileImage"
 
 export default function RoverProfile(props){
-    return (
-        <Link to={`/rovers/${props.name.toLowerCase()}/`} className="rover mb-5">
-            <h1 className="rover--header">{props.name}</h1>
+    const statusIndicatorClasses = {
+        "active": "spinner-grow spinner-grow-sm mt-2",
+        "complete": "ri-checkbox-blank-circle-fill ri-xs mt-1",
+        "unknown": "ri-checkbox-blank-circle-fill ri-xs mt-1",
+    }
+    const statusColorClass = {
+        "active": "text-success-bright",
+        "complete": "text-danger",
+        "unknown": "text-warning",
+    }
 
+    const statusIndicatorElement = <>
+        <h3 className={`d-flex mt-2 justify-content-center ${statusColorClass[props.rover.status]}`}>
+            <div className={statusIndicatorClasses[props.rover.status]}></div>
+            <span className="ms-2">Status: {props.rover.status.toUpperCase()}</span>
+        </h3>
+    </>
+
+    return (
+        <Link to={`/rovers/${props.name.toLowerCase()}/`} className="rover mb-3">
+            <h1 className="rover--header">{props.name}</h1>
             <img src={props.img} className="rounded-3"></img>
+
+            {statusIndicatorElement}
+            {props.rover.status == "complete" && <h4>Last Contact: {props.rover.max_date}</h4>}
         </Link>
     )
 }
 
 RoverProfile.defaultProps = {
-    img: roverProfileImageData.default
+    img: roverProfileImageData.default,
+    rover: {
+        status: "unknown"
+    }
 }
