a00cc09395cc659b7c180d29ee2dd017b2cb4feb
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-09-22 11:46:26 -0400

    Base RoverDetail component

---
+++ src/pages/RoverDetail.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function RoverSelect(props){
+    return (
+        <div className="h-100">
+            <h1>Rover Detail Page</h1>
+        </div>
+    )
+}
