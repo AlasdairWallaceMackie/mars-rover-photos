09dab5325676022584dfd1d34d07eb4fff79377e
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-09-23 09:40:15 -0400

    Add load screen. Won't render until data fetched

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from "react"
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route, Navigate} from "react-router-dom"
 
+import { RoverContext } from "./data/roverContext"
 import Home from "./pages/Home"
 import RoverDetail from "./pages/RoverDetail"
 
 
 export default function App(){
+    const {roverData} = React.useContext(RoverContext)
+
     return (
-        <main className="p-4 bg-space text-light">
-            <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route exact path="/rovers/:id" element={<RoverDetail />}/>
-            </Routes>
-        </main>
+        roverData.rovers ? 
+            <main className="p-4 bg-space text-light">
+                    <Routes>
+                        <Route exact path="/" element={<Home />} />
+                        <Route exact path="/rovers/" element={ <Navigate to="/" /> } />
+                        <Route exact path="/rovers/:name" element={<RoverDetail />}/>
+                    </Routes>
+            </main> :
+            <div className="loading-screen bg-space">
+                <div className="spinner-border text-light" role="status"></div>
+            </div>
     )
 }
