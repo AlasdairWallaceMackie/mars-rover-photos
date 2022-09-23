106530ad5aa1ea446e8f9417dfa0fe849821a7c1
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-09-23 09:39:10 -0400

    Each RoverDetail page now pulls context data

--- src/pages/RoverDetail.js
+++ src/pages/RoverDetail.js
@@ -1,9 +1,30 @@
 import React from "react"
+import {Link, useParams} from "react-router-dom"
+
+import {RoverContext} from "../data/roverContext"
 
 export default function RoverSelect(props){
+    const {roverData} = React.useContext(RoverContext)
+    const {name} = useParams()
+
+    const rover = roverData.rovers.find(r => r.name.toLowerCase() === name)
+
+    console.log("ROVER:")
+    console.log(rover)
+
     return (
-        <div className="h-100">
-            <h1>Rover Detail Page</h1>
+        <div className="full-height">
+            {rover ? 
+                <div>
+                    <h1>Rover Detail Page</h1>
+                    <h1>{rover.name}</h1>
+                </div> :
+                <div>
+                    <h1>Rover Data Not Found</h1>
+                    <Link to="/"><button className="btn btn-secondary">Back to Home</button></Link>
+                </div>
+            }
         </div>
+        
     )
 }
