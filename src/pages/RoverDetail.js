7ffb0291742202f779c90b3fa0cf6843447107c0
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-09-26 16:53:05 -0400

    Generate table on RoverDetail page

--- src/pages/RoverDetail.js
+++ src/pages/RoverDetail.js
@@ -16,8 +16,29 @@ export default function RoverSelect(props){
         <div className="full-height">
             {rover ? 
                 <div>
+                    <Link to="/" className="animated-arrow animated-arrow-left">
+                        <i className="ri-arrow-left-s-line ri-3x d-inline-block" />
+                        <span className="fs-2">Back</span>
+                    </Link>
                     <h1>Rover Detail Page</h1>
                     <h1>{rover.name}</h1>
+
+                    <table className="table text-light">
+                        <tbody>
+                            <tr>
+                                <td>Launch Date</td>
+                                <td>{rover.launch_date}</td>
+                            </tr>
+                            <tr>
+                                <td>Landing Date</td>
+                                <td>{rover.landing_date}</td>
+                            </tr>
+                            <tr>
+                                <td>Martian Days</td>
+                                <td>{rover.max_sol}</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div> :
                 <div>
                     <h1>Rover Data Not Found</h1>
