2ebf0b1513f425ee490173c86bbaf4e2c56ad0e1
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-09-27 11:06:11 -0400

    Feature: filter by Earth date and view photos

--- src/pages/RoverDetail.js
+++ src/pages/RoverDetail.js
@@ -6,11 +6,41 @@ import {RoverContext} from "../data/roverContext"
 export default function RoverSelect(props){
     const {roverData} = React.useContext(RoverContext)
     const {name} = useParams()
-
     const rover = roverData.rovers.find(r => r.name.toLowerCase() === name)
 
-    console.log("ROVER:")
-    console.log(rover)
+    const [earthDate, setEarthDate] = React.useState("")
+    const [photoData, setPhotoData] = React.useState([])
+    let photoDataElements = <h2>Select an Earth date to get photographs</h2>
+
+
+
+    // console.log(rover)
+
+    function handleChange(event){
+        setEarthDate(event.target.value)
+    }
+
+    function handleSubmit(event){
+        event.preventDefault()
+
+        fetch(`https://mars-photos.herokuapp.com/api/v1/rovers/${rover.name}/photos?earth_date=${earthDate}`)
+            .then(response => response.json())
+            .then(data => setPhotoData(data.photos))
+    }
+
+    console.log("EARTH DATE: " + earthDate)
+    console.log("Photo Data: " + JSON.stringify(photoData))
+
+    if (photoData.length){
+        photoDataElements = photoData.map(photo => (
+            <div>
+                <p>ID: {photo.id} -- {photo.camera.full_name}</p>
+                <img src={photo.img_src} style={{width: "500px"}}/>
+                <hr className="hr" />
+            </div>
+        ))
+    }
+
 
     return (
         <div className="full-height">
@@ -34,11 +64,36 @@ export default function RoverSelect(props){
                                 <td>{rover.landing_date}</td>
                             </tr>
                             <tr>
-                                <td>Martian Days</td>
+                                <td>Martian Days on the planet</td>
                                 <td>{rover.max_sol}</td>
+                            </tr>
+                            <tr>
+                                <td>Photos Available</td>
+                                <td>{rover.total_photos}</td>
                             </tr>
                         </tbody>
                     </table>
+
+                    <br/>
+
+                    <form onSubmit={handleSubmit}>
+                        <h1>PHOTOS</h1>
+
+                        <div className="d-flex mb-5">
+                            <input
+                                type="date"
+                                min={rover.launch_date}
+                                max={rover.max_date}
+                                onChange={event => handleChange(event)}
+                                required={true}
+                                className="form-control me-3"
+                                style={{width: "200px"}}
+                            />
+                            <input type="submit" className="btn btn-secondary"/>
+                        </div>
+                    </form>
+
+                    {photoDataElements}
                 </div> :
                 <div>
                     <h1>Rover Data Not Found</h1>
