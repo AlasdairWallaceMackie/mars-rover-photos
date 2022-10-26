85807c271b6a3476b2cdf6f61d3aa6d3f189ca3e
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-10-26 12:51:57 -0400

    Add nanoid key to roverLinkElements

--- src/components/RoverNav.js
+++ src/components/RoverNav.js
@@ -1,12 +1,13 @@
 import React from "react"
 import {Link} from "react-router-dom"
+import {nanoid} from "nanoid"
 
 import {RoverContext} from "../data/roverContext"
 
 export default function RoverNav(props){
 
     const roverLinkElements = props.roverData.rovers.map(rover => (
-        <li><Link
+        <li key={nanoid()}><Link
             to={`/rovers/${rover.name.toLowerCase()}`}
             className={`dropdown-item ${rover.name.toLowerCase()===props.currentRover ? "disabled" : ""}`}
         >
@@ -15,12 +16,12 @@ export default function RoverNav(props){
     ))
 
     return (
-        <div id="rover-nav" class="dropdown">
-            <a class="btn btn-lg btn-secondary dropdown-toggle" href="#" id="dropdownMenuLink" data-bs-toggle="dropdown">
+        <div id="rover-nav" className="dropdown">
+            <a className="btn btn-lg btn-secondary dropdown-toggle" href="#" id="dropdownMenuLink" data-bs-toggle="dropdown">
                 Rovers
             </a>
 
-            <ul class="dropdown-menu">
+            <ul className="dropdown-menu">
                 {roverLinkElements}
             </ul>
         </div>
