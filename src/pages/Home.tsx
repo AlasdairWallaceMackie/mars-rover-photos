0e5a2b72fb3ac1323d99ab4e50a0f7f9153b7e56
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-11-10 10:57:52 -0500

    Small HTML formatting change for readability

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,10 @@ import RoverSelect from "../components/RoverSelect"
 
 export default function Home(){
     const {roverData} = React.useContext(RoverContext)
-    const waitMessage = <h1 className="text-center">Fetching Data... <div className="spinner-border text-light" role="status"></div></h1>
+    const waitMessage = <h1 className="text-center">
+        Fetching Data... 
+        <div className="spinner-border text-light" role="status"></div>
+    </h1>
     
     return (
         <>
